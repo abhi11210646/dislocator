95395378aabc5e58691a0a6b9e7cad38b01d5da7
Abhishek Kumar Singh <jonu.1504@gmail.com>
2020-05-18 20:01:40 +0530

    Test cases: covered with test deno case (#2)
    * First test added
    
    * covered with test cases
    
    Co-authored-by: Abhishek Singh <abhisekhs@one.com>

--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
 const nameWhitelist = new RegExp("^[a-z0-9]+$", "i");
 
 export default class Dislocator {
-  _registry: any = new Map();
-  _instances: any = new Map();
-  _loading: any[] = [];
+  private registry: any = new Map();
+  private instances: any = new Map();
+  private loading: any[] = [];
 
   register(name: string, callback: any) {
     if (!nameWhitelist.test(name)) {
@@ -12,7 +12,7 @@ export default class Dislocator {
     if (this.isRegistered(name)) {
       throw new Error(`A service called "${name}" is already registered.`);
     }
-    this._registry.set(name, callback);
+    this.registry.set(name, callback);
     return this;
   }
 
@@ -25,39 +25,39 @@ export default class Dislocator {
     if (!this.isRegistered(name)) {
       throw new Error(`No registration named "${name}"`);
     }
-    this._registry.delete(name);
-    this._instances.delete(name);
+    this.registry.delete(name);
+    this.instances.delete(name);
     return this;
   }
 
   isRegistered(name: string) {
-    return !!this._registry.get(name);
+    return !!this.registry.get(name);
   }
 
   get(name: string) {
-    if (this._loading.indexOf(name) !== -1) {
-      const chain = this._loading.reduce((str: string, name: string) =>
+    if (this.loading.indexOf(name) !== -1) {
+      const chain = this.loading.reduce((str: string, name: string) =>
         `${str} -> ${name}`
       );
       throw new Error(`Circular dependency detected (${chain} -> ${name})`);
     }
 
-    this._loading.push(name);
+    this.loading.push(name);
 
     if (!this.isRegistered(name)) {
       throw new Error(`No registration named "${name}"`);
     }
 
-    if (!(this._instances.has(name))) {
-      if (typeof this._registry.get(name) === "function") {
-        this._instances.set(name, this._registry.get(name)(this));
+    if (!(this.instances.has(name))) {
+      if (typeof this.registry.get(name) === "function") {
+        this.instances.set(name, this.registry.get(name)(this));
       } else {
-        this._instances.set(name, this._registry.get(name));
+        this.instances.set(name, this.registry.get(name));
       }
     }
 
-    this._loading.pop();
+    this.loading.pop();
 
-    return this._instances.get(name);
+    return this.instances.get(name);
   }
 }
